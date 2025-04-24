cf44f6c94b22ad89da2ee3dacbe363c6690a1586
Sirolbfr <briquet.loris@gmail.com>
2025-04-24 16:30:39 +0200

    finished

---
+++ script.js
@@ -0,0 +1,71 @@
+/* -------- Constants -------- */
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const digits = "0123456789";
+const special = "!@#$%^*()_+[]{}|;:,.?/`~-=\\'";
+
+const upp = document.getElementById("ckbox_upp");
+const low = document.getElementById("ckbox_low");
+const dig = document.getElementById("ckbox_dig");
+const spe = document.getElementById("ckbox_spe");
+
+const input = document.getElementById("input");
+const display = document.getElementById("display");
+
+
+/* -------- Main -------- */
+
+function selection() {
+    let selected = "";
+
+    if (upp.checked==true) {selected += uppercase;}
+    if (low.checked==true) {selected += lowercase;}
+    if (dig.checked==true) {selected += digits;}
+    if (spe.checked==true) {selected += special;}
+
+    return selected;
+}
+
+
+
+function pswrd_gen() {
+    let res = "";
+    let input_length = Number(input.value);
+    let cp_btn = document.getElementById("cp_btn");
+    if (cp_btn!=undefined) {cp_btn.remove();}
+
+
+    if (selection().length>0) {
+        if (input_length < Number(input.min)) {res = 'Nombre de caractères trop bas';}
+        else if (input_length > Number(input.max)) {res = 'Nombre de caractères trop haut';}
+        else {
+            for (let i=0; i<input_length; i++) {
+                let rndm = Math.floor(Math.random()* selection().length);
+                res += selection().charAt(rndm);
+            }
+            
+            cp_btn = document.createElement("button");
+            cp_btn.className="material-symbols-outlined";
+            cp_btn.id="cp_btn";
+            cp_btn.setAttribute("onclick", "copy();");
+            cp_btn.innerHTML="content_copy";
+            document.getElementById("display_cont").appendChild(cp_btn);
+        }
+    } else {res = 'Veuillez cocher au moins une des cases de la partie 2.';}
+    
+    display.innerHTML = res.toString();
+}
+
+function copy() {
+    const output = document.getElementById("display").innerHTML;
+    navigator.clipboard.writeText(output);
+    alert("Mot de passe copié dans le presse-papier");
+}
+
+
+/* --------- Testing on 'Enter' key press --------- */
+
+input.onkeydown = function(key){
+    if(key.keyCode == 13){pswrd_gen();}
+};
